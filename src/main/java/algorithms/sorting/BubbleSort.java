6daaa5b93bb6e4e77ba75223a6f8eaeb22afe4b1
rakesh kapri <rakeshkapri@gmail.com>
2020-01-04 23:10:11 +0000

    Added algorithm : selection sort

--- src/main/java/algorithms/sorting/BubbleSort.java
+++ src/main/java/algorithms/sorting/BubbleSort.java
@@ -9,7 +9,7 @@ public class BubbleSort {
 
     private static void bubbleSort(int[] arr) {
         for(int i = 0; i< arr.length; i++){
-            for(int j = 0; j< arr.length - i - 1; j++){
+            for(int j = 0; j < arr.length - i - 1; j++){
                 if(arr[j] > arr[j+1]){
                     swap(j, j+1, arr);
                 }
---
+++ src/main/java/algorithms/sorting/SelectionSort.java
@@ -0,0 +1,23 @@
+package algorithms.sorting;
+
+import static algorithms.common.SortingUtils.*;
+
+public class SelectionSort {
+    public static void main(String s[]){
+        int[] arr = scanInput();
+        selectionSort(arr);
+        print(arr);
+    }
+
+    private static void selectionSort(int[] arr){
+        for (int i = 0; i< arr.length; i++){
+            int max = 0;
+            for(int j = i + 1; j < arr.length -i -1; j++){
+                if(arr[j] > max){
+                    max = j;
+                }
+            }
+            swap(max, arr.length - i -1, arr);
+        }
+    }
+}
